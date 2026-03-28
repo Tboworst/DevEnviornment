eb5db68b55891fbbe32c994174041c188d96e6d9
Thibault <156127071+Tboworst@users.noreply.github.com>
2026-03-27 23:58:00 -0400

    Added databases and some inquirer.py

---
+++ databases.py
@@ -0,0 +1,28 @@
+
+def generate_postgres():
+    return """  
+    import psycopg2
+
+  conn = psycopg2.connect(
+      host="localhost",
+      database="mydb",
+      user="postgres",
+      password="password"
+  )
+
+  cursor = conn.cursor()
+
+
+
+"""
+def generate_Sqlite():
+    return """
+  import sqlite3
+
+  conn = sqlite3.connect("mydb.db")
+  cursor = conn.cursor()
+
+
+
+"""
+
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from InquirerPy import inquirer
 import click
 from pathlib import Path
 import languages
+import databases
 
-@click.command()
-@click.argument('name')
-# --backend takes a value like "go", "java", "ts", "python" — default is python
-@click.option("--backend", default="python", help="Language for the backend: go, java, ts, python")
+
+
 def create_app(name, backend):
     # Build the folder path from the project name the user gave
+    backend = inquirer.select(
+        message= "Select a backend language:",
+        choices=["go","python","java","Typescript"]
+    ).execute()
+    
+    
+    database = inquirer.checkbox(
+        message= "Select your database:",
+        choices= ["Postgres","Sqlite"]
+    ).execute()
+
     folder = Path(name)
 
     if folder.exists():
@@ -22,12 +33,18 @@ def create_app(name, backend):
         "python": languages.generate_python,
     }
 
+    databas = {
+        "PostGres": databases.generate_postgres,
+        "Sqlite" : databases.generate_Sqlite
+    }
+
     # checks if the users input has an actuall backend in the map if not returns
     if backend not in generators:
         click.echo(f"Unknown backend: {backend}. Choose from: go, java, ts, python")
         return
 
-    # Call the matching generator function to get the file content as a string
+    # Call the matching generator function to get the file content as a string,\
+    #must put parantheses at the end b/c when using map we cannot put function so the paranthese allows whats called to bea function u 
     content = generators[backend]()
 
     # Map each language to its correct file extension
@@ -37,7 +54,10 @@ def create_app(name, backend):
         "ts": "ts",
         "python": "py",
     }
+    #saved insde a varaible so that it can be used in the f string 
     ext = extensions[backend]
+
+    
 
     # Create the project folder
     folder.mkdir()
@@ -46,10 +66,13 @@ def create_app(name, backend):
     (folder / f"main.{ext}").write_text(content)
 
     (folder / "README.md").write_text(
-        f"# {name}\nCreated with --backend {backend}"
+        f"# {name}\nCreated with  {backend}"
     )
 
     click.echo(f"Created project: {name} with {backend} backend")
+
+
+
 
 if __name__ == '__main__':
     create_app()
